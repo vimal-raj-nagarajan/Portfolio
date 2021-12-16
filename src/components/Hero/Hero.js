a753803325a6fbea9bba3b6673b4bccfc46a27d9
Vimal Raj <vimalraj.nagarajan@gmail.com>
2021-12-16 21:15:11 +1300

    Add  Hero Section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,20 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello!<br />
+        I'm Vimal<br />
+        Full-stack developer
+      </SectionTitle>
+      <SectionText>
+      My main goal is to have a positive impact on whatever project I'm involved in.
+      </SectionText>
+      <Button onClick={() => window.location = 'mailto:vimalraj.nagarajan@gmail.com'}>Contact Me</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
