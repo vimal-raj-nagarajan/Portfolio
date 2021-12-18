761ed3d0b6ad808bfe749f4f4f357dd24a0e5a5c
Vimal Raj <vimalraj.nagarajan@gmail.com>
2021-12-18 23:18:52 +1300

    Add correct images

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,6 @@
 export const projects = [
   {
-    title: 'Real Estate Platform',
+    title: 'Pupparazzi',
     description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
       image: '/images/1.png',
       tags: ['Mongo', 'Express', 'React', 'Node'],
@@ -20,7 +20,7 @@ export const projects = [
   {
     title: 'KOIOS',
     description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-      image: '/images/3.jpg',
+      image: '/images/3.png',
       tags: ['React', 'WebRTC'],
     source: 'https://github.com/vimal-raj-nagarajan',
     visit: 'https://google.com',
@@ -29,7 +29,7 @@ export const projects = [
   {
     title: 'Pizza Shop',
     description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-    image: '/images/4.jpg',
+    image: '/images/4.png',
     tags: ['React', 'ChatEngine', 'Firebase'],
     source: 'https://github.com/vimal-raj-nagarajan',
     visit: 'https://google.com',
